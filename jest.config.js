e84deb2fff1a7c7a14ad5eb177a36b919420155b
jaasimjalal <jaasim012@gmail.com>
2026-01-12 17:42:10 +0300

    Add project configuration and dependencies

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'node',
+  verbose: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  testMatch: ['**/tests/**/*.test.js'],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
+  testTimeout: 10000,
+  clearMocks: true,
+  restoreMocks: true,
+};
